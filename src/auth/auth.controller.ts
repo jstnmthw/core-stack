5dd5e12d9de52c5a3e8cf2e2a48f81dd17a6e38f
Justin <web@jstn.ly>
2024-10-15 01:48:07 +0700

    Refactor auth controller and add JWT authentication guard

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Post, Request, UseGuards } from '@nestjs/common';
+import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
 import { LocalAuthGuard } from './local-auth.guard';
 import { User } from 'src/users/user.entity';
 import { AuthService } from './auth.service';
+import { JwtAuthGuard } from './jwt-auth.guard';
 
 interface UserRequest extends Request {
   user: User;
@@ -15,5 +16,11 @@ export class AuthController {
   @Post('login')
   async login(@Request() req: UserRequest) {
     return this.authService.login(req.user);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('me')
+  getProfile(@Request() req: UserRequest) {
+    return req.user;
   }
 }
