b1907b8fba2b07bfd2e2a3ea894f299c0b889270
Justin <web@jstn.ly>
2024-10-15 01:54:14 +0700

    Refactor JWT token expiration time to 24 hours

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -16,7 +16,7 @@ import { JwtStrategy } from './jwt.strategy';
       inject: [ConfigService],
       useFactory: async (configService: ConfigService) => ({
         secret: configService.get('JWT_SECRET'),
-        signOptions: { expiresIn: '60s' },
+        signOptions: { expiresIn: '24h' },
       }),
     }),
   ],
