706ca62a81d7ad07e1670b691884ec7f9ec4c8fe
Justin <web@jstn.ly>
2024-10-16 22:39:47 +0700

    Refactor roles.module.ts to import and use the IsNameUniqueConstraint validator

--- src/roles/roles.module.ts
+++ src/roles/roles.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { RolesService } from './roles.service';
 import { RolesController } from './roles.controller';
+import { IsNameUniqueConstraint } from './validators/is-name-unique';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Role } from './entities/role.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Role])],
-  providers: [RolesService],
+  providers: [RolesService, IsNameUniqueConstraint],
   controllers: [RolesController],
   exports: [RolesService],
 })
